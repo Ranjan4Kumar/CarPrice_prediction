b9f06f60d1f87f364417b8840785170eb7a49a77
Your Name <you@example.com>
2023-01-08 14:10:22 +0530

    new update

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 
 app = Flask(__name__)
@@ -19,6 +19,17 @@ def index():
     fuel_type = car['fuel_type'].unique()
 
     return render_template('index.html', companies=companies, car_models=car_model, years=year, fuel_types=fuel_type)
+
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     company = request.form.get('company')
+#     car_model = request.form.get('car_models')
+#     year = request.form.get('year')
+#     fuel_type = request.form.get('fuel_type')
+#     kms_driven = request.form.get('kilo_driven')
+#     print(company, car_model, year, fuel_type, kms_driven)
+#     return ""
 
 
 if __name__ == "__main__":
